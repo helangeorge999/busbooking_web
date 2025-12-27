135ef619b44d751694cba832b389d6351b63a819
helangeorge999 <helangeorge78@gmail.com>
2025-12-27 12:42:43 +0545

    update register/page.tsx code

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -6,8 +6,7 @@ export default function Page() {
     return (
         <div className="space-y-6 w-full">
             <div className="text-center">
-                <h1 className="text-2xl font-semibold">Create your account</h1>
-                <p className="mt-1 text-sm text-foreground/70">Sign up to get started</p>
+                <h1 className="text-2xl font-semibold">Register</h1>
             </div>
             <RegisterForm />
         </div>
