a530b65ae518285492060915ab44195b3f11c4b7
helangeorge999 <helangeorge78@gmail.com>
2025-12-27 12:03:05 +0545

    update dashboard page and import bus image

--- app/(auth)/schema.ts
+++ app/(auth)/schema.ts
@@ -1,20 +1,25 @@
-import z from "zod";
+import { z } from "zod";
+
+/* ---------------- LOGIN ---------------- */
 
 export const loginSchema = z.object({
-    email: z.email({ message: "Enter a valid email" }),
-    password: z.string().min(6, { message: "Minimum 6 characters" }),
+    email: z.string().email("Invalid email address"),
+    password: z.string().min(6, "Password must be at least 6 characters"),
 });
 
 export type LoginData = z.infer<typeof loginSchema>;
 
+/*REGISTER */
+
 export const registerSchema = z.object({
-    name: z.string().min(2, { message: "Enter your name" }),
-    email: z.email({ message: "Enter a valid email" }),
-    password: z.string().min(6, { message: "Minimum 6 characters" }),
-    confirmPassword: z.string().min(6, { message: "Minimum 6 characters" }),
-}).refine((v) => v.password === v.confirmPassword, {
-        path: ["confirmPassword"],
-        message: "Passwords do not match",
+    firstName: z.string().min(2, "First name must be at least 2 characters"),
+    lastName: z.string().min(2, "Last name must be at least 2 characters"),
+    email: z.string().email("Invalid email address"),
+    password: z.string().min(6, "Password must be at least 6 characters"),
+    confirmPassword: z.string().min(6, "Confirm password is required"),
+}).refine((data) => data.password === data.confirmPassword, {
+    path: ["confirmPassword"],
+    message: "Passwords do not match",
 });
 
 export type RegisterData = z.infer<typeof registerSchema>;
